df821f88da24fe00eb2e6826cd91b25243f5733b
Md-Sifat-code <codewithsifat4@gmail.com>
2025-01-13 22:24:04 +0600

    added the left side details

--- src/sidebars/Left_side.jsx
+++ src/sidebars/Left_side.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Left_side() {
-  return <div>Left_side</div>;
+  return (
+    <section>
+      <div className="flex flex-col items-center justify-center h-full">
+        {/* a;; icons part */}
+        <div>
+          <h1 className="text-3xl rob mt-2 font-bold text-center text-white">
+            Daily<span className="textcol text-3xl font-extrabold">Tech</span>
+          </h1>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
 }
